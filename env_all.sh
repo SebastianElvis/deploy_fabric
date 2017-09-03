a4c35d3f3d41cb75fc717989d701f4aead0685d9
SebastianElvis <408063141@qq.com>
2017-09-03 12:42:16 +0100

    0903

--- env_all.sh
+++ env_all.sh
@@ -2,6 +2,6 @@
 
 USER_NAME=hanrc
 USER_HOME=/users/$USER_NAME
-EXTSTORAGE=$USER_HOME/extstorage
-FABRIC_DIR=$GOPATH/src/github.com/hyperledger/fabric
+EXTSTORAGE=$USER_HOME/hanrc
+FABRIC_DIR=$EXTSTORAGE/go/src/github.com/hyperledger/fabric # GOPATH is EXTSTORAGE/go
 DEPLOY_FABRIC_DIR=$EXTSTORAGE/deploy_fabric
