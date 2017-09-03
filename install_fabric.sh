a8ff2fe4eaf01764f5df3494e6c5ec4e5ce78a7b
Runchao Han <hanrc@node-8.hl8.blockchainanom.emulab.net>
2017-09-03 05:37:43 -0600

    0903-2

--- install_fabric.sh
+++ install_fabric.sh
@@ -1,3 +1,6 @@
+sudo apt install docker-compose
+mkdir -p $GOPATH/src/github.com
+
 sudo rm -Rf $GOPATH/src/github.com/hyperledger 
 mkdir -pv $GOPATH/src/github.com/hyperledger 
 cd $GOPATH/src/github.com/hyperledger 
@@ -7,17 +10,20 @@ git clone https://github.com/hyperledger/fabric-ca
 cd $GOPATH/src/github.com/hyperledger/fabric 
 git branch --all 
 git fetch
-make clean
-make dist-clean
-make configtxgen
-make docker
-make peer-docker
-make orderer-docker
-make couchdb
+sudo make clean
+sudo make dist-clean
+sudo make configtxgen
+sudo make docker
+sudo make peer-docker
+sudo make orderer-docker
+sudo make couchdb
+sudo make peer
 
 cd $GOPATH/src/github.com/hyperledger/fabric-ca
 git branch --all
 git fetch 
-make clean
-make dist-clean
-make docker 
+sudo make clean
+sudo make dist-clean
+sudo make docker
+sudo make fabric-ca-server
+sudo make fabric-ca-client 
